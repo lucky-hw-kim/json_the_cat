04f5789db7012aa78b346f287d011d2b91942959
Lucky Kim <lucky.hw.kim@gmail.com>
2022-03-17 15:08:12 +0000

    fix bugs

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,26 @@
-const breed = process.argv[2]
 const request = require('request')
 const URL = 'https://api.thecatapi.com/v1/breeds/search?q=${breed}'
-request(URL, (error, response, body) => {
-  if (error) {
-    console.log('Error 404: Wrong URL!!!...Meow')
-    return
-  }
-  const data = JSON.parse(body)
-  if (data.length !== 0) {
-    console.log('statusCode:', response && response.statusCode)
-    console.log('data:', data)
-  // console.log(typeof data);
-  } else if (breed === undefined) {
-    console.log('Error 400:', 'Type the breed....Meow')
-  } else if (data.length === 0) {
-    console.log('Error 204:', `There is no CAT breed called ${breed}....Meow`)
-  }
-})
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(URL, (error, response, body) => {
+    if (error) {
+      console.log('Error 404: Wrong URL!!!...Meow')
+      return
+    }
+    const data = JSON.parse(body)
+    if (data.length !== 0) {
+      // console.log('statusCode:', response && response.statusCode)
+      // console.log('data:', data)
+      return `statusCode: ${response && response.statusCode} \n data: ${data} `
+    // console.log(typeof data);
+    } else if (breed === undefined) {
+      //console.log('Error 400:', 'Type the breed....Meow')
+      return 'Error 400:', 'Type the breed....Meow'
+    } else if (data.length === 0) {
+      //console.log('Error 204:', `There is no CAT breed called ${breed}....Meow`)
+      return 'Error 204:', `There is no CAT breed called ${breed}....Meow`
+    }
+  })
+};
+
+ module.exposts = {fetchBreedDescription};
